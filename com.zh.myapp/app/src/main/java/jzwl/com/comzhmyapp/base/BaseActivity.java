003c1dfe2a85a8a232a826b7526f4e71c78cf137
zhaoh <zhaoh@126.com>
2018-06-01 18:06:44 +0800

    -----------------------

---
+++ com.zh.myapp/app/src/main/java/jzwl/com/comzhmyapp/base/BaseActivity.java
@@ -0,0 +1,15 @@
+package jzwl.com.comzhmyapp.base;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * 创建日期：2018/6/1
+ * 描述: 基类的Activity
+ *
+ * @author: zhaoh
+ */
+public abstract class BaseActivity extends AppCompatActivity{
+
+
+
+}
---
+++ com.zh.myapp/app/src/main/java/jzwl/com/comzhmyapp/ui/activity/TestQuanZhongActivity.java
@@ -0,0 +1,12 @@
+package jzwl.com.comzhmyapp.ui.activity;
+
+import android.app.Activity;
+
+/**
+ * 创建日期：2018/5/21
+ * 描述:
+ *
+ * @author: zhaoh
+ */
+public class TestQuanZhongActivity extends Activity {
+}
